e94ba8bcc5746ba04cf64a6fc7bf13a305dcc47a
Alvin1Sun <124801208+Alvin1Sun@users.noreply.github.com>
2025-10-29 17:27:55 +0800

    Update bit calculations

--- tools/params.go
+++ tools/params.go
@@ -4,14 +4,14 @@
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
 //
-//  1. Redistributions of source code must retain the above copyright
-//     notice, this list of conditions and the following disclaimer.
-//  2. Redistributions in binary form must reproduce the above copyright
-//     notice, this list of conditions and the following disclaimer in the
-//     documentation and/or other materials provided with the distribution.
-//  3. Neither the name of the copyright holder nor the
-//     names of its contributors may be used to endorse or promote products
-//     derived from this software without specific prior written permission.
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. Neither the name of the copyright holder nor the
+//    names of its contributors may be used to endorse or promote products
+//    derived from this software without specific prior written permission.
 //
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 // ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -23,6 +23,7 @@
 // ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 package tools
 
 import (
@@ -75,9 +76,13 @@ func (paramsraw *ParamsRaw) Compile(option int) Params {
 		params.HashLen = paramsraw.HashLen
 		params.PayloadLen = paramsraw.PayloadLen
 		params.MaxBits = paramsraw.HashLen + paramsraw.PayloadLen
+
+		// Calculate how many complete 11-bit packages we can fit
 		elevenbits_num := params.MaxBits / 11
 		resbits := params.MaxBits - elevenbits_num*11
 		params.Seven3num = elevenbits_num
+
+		// Calculate how many trits can fit in the remaining bits
 		params.Res3num = 0
 		if resbits > 0 {
 			temp := 1
@@ -85,13 +90,18 @@ func (paramsraw *ParamsRaw) Compile(option int) Params {
 				if temp < (1 << resbits) {
 					params.Res3num += 1
 					temp *= 3
+				} else {
+					break
 				}
 			}
 		}
+
 		params.MaxDepth = params.Seven3num*7 + params.Res3num
 		params.MaxHashPackage = 2
 		Maxhash0 := params.MaxHashPackage * 11
+
 		if paramsraw.HashLen <= Maxhash0 {
+			// Single hash segment
 			params.HashLenSet = make([]int, 1)
 			params.HashLenSet[0] = paramsraw.HashLen
 			params.PayloadLenSet = make([]int, 1)
@@ -101,31 +111,66 @@ func (paramsraw *ParamsRaw) Compile(option int) Params {
 			params.HashMask0 = (1 << params.HashLenSet[0]) - 1
 			params.HashMask1 = 0
 		} else {
-			params.HashLenSet = make([]int, params.Seven3num-params.MaxHashPackage+1)
-			params.HashLenSet[0] = Maxhash0
+			// Multiple hash segments
 			hashres := params.HashLen - Maxhash0
-			hash1 := hashres / (params.Seven3num - params.MaxHashPackage)
-			// fmt.Println(params.HashLen, Maxhash0, hashres, params.Seven3num-2)
-			if hash1*(params.Seven3num-params.MaxHashPackage) != hashres {
-				log.Fatal("Invalid Hash Length!")
+			numSegments := params.Seven3num - params.MaxHashPackage
+
+			if numSegments <= 0 {
+				log.Fatal("Invalid configuration: Not enough segments for hash distribution!")
 			}
-			params.PayloadLenSet = make([]int, params.Seven3num-params.MaxHashPackage+1)
+
+			// Calculate average hash length per segment
+			hash1 := (hashres + numSegments - 1) / numSegments
+
+			if hash1*numSegments > hashres {
+				hash1 = hashres / numSegments
+			}
+
+			remainingHash := hashres - hash1*numSegments
+
+			params.HashLenSet = make([]int, numSegments+1)
+			params.HashLenSet[0] = Maxhash0
+
+			params.PayloadLenSet = make([]int, numSegments+1)
 			params.PayloadLenSet[0] = resbits
-			for i := 1; i < params.Seven3num-params.MaxHashPackage+1; i++ {
+
+			// Distribute hash lengths and payload lengths across segments
+			for i := 1; i <= numSegments; i++ {
 				params.HashLenSet[i] = hash1
-				params.PayloadLenSet[i] = 11 - hash1
+				if i <= remainingHash {
+					params.HashLenSet[i] += 1
+				}
+				params.PayloadLenSet[i] = 11 - params.HashLenSet[i]
 			}
-			params.CheckPoint = make([]int, params.Seven3num-params.MaxHashPackage+1)
+
+			totalHashBits := 0
+			totalPayloadBits := 0
+			for i := 0; i < len(params.HashLenSet); i++ {
+				totalHashBits += params.HashLenSet[i]
+				totalPayloadBits += params.PayloadLenSet[i]
+			}
+
+			if totalHashBits != params.HashLen {
+				log.Fatalf("Hash bit allocation error: allocated %d bits but need %d bits", totalHashBits, params.HashLen)
+			}
+
+			if totalPayloadBits != params.PayloadLen {
+				log.Fatalf("Payload bit allocation error: allocated %d bits but need %d bits", totalPayloadBits, params.PayloadLen)
+			}
+
+			params.CheckPoint = make([]int, numSegments+1)
 			params.CheckPoint[0] = params.MaxDepth
-			for i := 1; i < params.Seven3num-params.MaxHashPackage+1; i++ {
+			for i := 1; i <= numSegments; i++ {
 				params.CheckPoint[i] = 7*params.MaxHashPackage + 7*i
 			}
+
 			params.HashMask0 = (1 << params.HashLenSet[0]) - 1
 			params.HashMask1 = (1 << hash1) - 1
 		}
 
-		// 5 * 11-bit ~ 35 * three
+		// 5 * 11-bit packages = 55 bits per int
 		params.Necessary_Decoding_Ints = (params.MaxBits + 54) / 55
+
 		params.PreviousNuc = HomopolymerLen
 		for i := 0; i < len(Exclude_motifs); i++ {
 			if params.PreviousNuc < len(Exclude_motifs[i])-1 {
@@ -137,11 +182,13 @@ func (paramsraw *ParamsRaw) Compile(option int) Params {
 				params.PreviousNuc = len(Exclude_long_motifs[i]) - 1
 			}
 		}
+
 		params.PatternMask = (1 << (2 * params.PreviousNuc)) - 1
 		params.GCrightshift = 2 * params.PreviousNuc
 		params.Depthrightshift = params.GCrightshift + 8
-		params.Indexrightshift = params.Depthrightshift + 8
-		params.Addressrightshift = params.Indexrightshift + 8
+		params.Indexrightshift = params.Depthrightshift + 9
+		params.Addressrightshift = params.Indexrightshift + 9
+
 		params.GCmax = 0.575
 		params.GCmin = 0.425
 		params.Balance_Bound = 1.0
@@ -151,6 +198,7 @@ func (paramsraw *ParamsRaw) Compile(option int) Params {
 		params.HashLen = paramsraw.HashLen
 		params.PayloadLen = paramsraw.PayloadLen
 		Maxhash0 := int(math.Ceil(float64(params.MaxDepth)*0.15)) + 5
+
 		if paramsraw.HashLen <= Maxhash0 {
 			params.HashLenSet = make([]int, 1)
 			params.HashLenSet[0] = paramsraw.HashLen
@@ -162,7 +210,7 @@ func (paramsraw *ParamsRaw) Compile(option int) Params {
 			params.HashMask1 = 0
 		} else {
 			extrahashnum := (paramsraw.HashLen - Maxhash0 + 4) / 5
-			r := paramsraw.HashLen - Maxhash0 - 5*(extrahashnum-1) // 1~5
+			r := paramsraw.HashLen - Maxhash0 - 5*(extrahashnum-1)
 			params.HashLenSet = make([]int, extrahashnum+1)
 			params.HashLenSet[0] = Maxhash0
 			params.HashLenSet[1] = r
@@ -196,6 +244,7 @@ func (paramsraw *ParamsRaw) Compile(option int) Params {
 			params.HashMask0 = (1 << params.HashLenSet[0]) - 1
 			params.HashMask1 = (1 << params.HashLenSet[1]) - 1
 		}
+
 		params.Necessary_Decoding_Ints = (params.MaxDepth + 63) / 64
 		params.PreviousNuc = HomopolymerLen
 		for i := 0; i < len(Exclude_motifs); i++ {
@@ -226,10 +275,8 @@ func (paramsraw *ParamsRaw) Compile(option int) Params {
 			params.GCmin = 0.425
 			params.Balance_Bound = 0.29
 		}
-
 	}
 	return params
-
 }
 
 const Gungnir_Default_Params = 0
@@ -250,5 +297,5 @@ const Uint9Mask = (1 << 9) - 1
 const Uint11Mask = (1 << 11) - 1
 const Uint14Mask = (1 << 14) - 1
 const Uint16Mask = (1 << 16) - 1
-const ShardCountDefaultLog = 12 // num: 1<<12 = 4096
+const ShardCountDefaultLog = 12
 const ShardMask = (1 << ShardCountDefaultLog) - 1
